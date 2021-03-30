b50ab02bbfde262858f41cff3e78952877a1aff2
peter <peterwilson.intake34@gmail.com>
2021-03-30 08:49:36 +0400

    [ADD] components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { OrderDetailsComponent } from './shopping-cart/order-details/order-details.component';
+import { UserDetailsComponent } from './shopping-cart/user-details/user-details.component';
+import { PaymentDetailsComponent } from './shopping-cart/payment-details/payment-details.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    OrderDetailsComponent,
+    UserDetailsComponent,
+    PaymentDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
